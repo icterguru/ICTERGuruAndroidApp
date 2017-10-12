0a11d20b031434f4766186c3ed41e1ef65019275
Mokter Hossain <mokter@gmail.com>
2017-10-12 10:43:26 -0400

    Initial commit of ICTERGuru AndroidApp

---
+++ app/src/main/java/com/icterguru/myaccount/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.icterguru.myaccount
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+
+/* Java Version of this codes
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+*/
